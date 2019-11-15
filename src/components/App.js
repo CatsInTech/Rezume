4ff5d99d419a2c19fff9f7165c556cb844de548e
Prajaram <prajaram345@gmail.com>
2019-11-15 20:56:32 +0000

    Removed unwanted files.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,9 @@
-import React from "react";
-import logo from "../assets/logo.svg";
-import "../styles/App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <>React Resume Generator</>;
+  }
 }
 
 export default App;
