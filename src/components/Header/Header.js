38aed39055690e0d8cfd5cf7563b151355b1745b
Prajaram <prajaram345@gmail.com>
2019-11-15 22:13:26 +0000

    Added header links.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <span className="navbar-brand mb-0 h1">React Resume Generator</span>
+    <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-between">
+      <span className="navbar-brand mb-0 h1 col-1">React Resume Generator</span>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <span className="nav-link">About</span>
+        </li>
+        <li className="nav-item">
+          <span className="nav-link">Sections</span>
+        </li>
+        <li className="nav-item">
+          <span className="nav-link">Contact</span>
+        </li>
+        <li className="nav-item">
+          <span className="nav-link">Design</span>
+        </li>
+      </ul>
+      <button className="btn btn-primary col-1">About Us</button>
     </nav>
   );
 };
