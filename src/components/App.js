c00635557a4f037452f366f7c18746ada8ebb22c
Prajaram <prajaram345@gmail.com>
2019-11-16 13:15:12 +0000

    Added a structure for resume.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,16 @@ import Main from "./Main/Main";
 
 class App extends Component {
   state = {
-    CurrentPage: "About"
+    CurrentPage: "About",
+    Resume: {
+      About: {
+        Name: "Priyadharshini Raja Ram",
+        Location: "London, United Kingdom",
+        CurrentPosition: "Product Owner at Cloudroit",
+        Summary:
+          "Strong proficiency in JavaScript, including DOM manipulation and the JavaScript object model and thorough understanding of React JS and its core principles. Experience with popular React.js workflows (such as Flux or Redux). Familiarity with newer specifications of EcmaScript."
+      }
+    }
   };
   ChangePage = CurrentPage => {
     this.setState({ CurrentPage });
@@ -16,7 +25,7 @@ class App extends Component {
           ChangePage={this.ChangePage}
           CurrentPage={this.state.CurrentPage}
         />
-        <Main CurrentPage={this.state.CurrentPage} />
+        <Main CurrentPage={this.state.CurrentPage} Resume={this.state.Resume} />
       </>
     );
   }
