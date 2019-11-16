f81ec0555f634b1350f725250608e15a84cb1a37
Prajaram <prajaram345@gmail.com>
2019-11-16 19:15:34 +0000

    Added contact information.

--- src/components/Rezume/Rezume.js
+++ src/components/Rezume/Rezume.js
@@ -1,10 +1,41 @@
 import React from "react";
 
 const Rezume = ({ Resume }) => {
-  const { Name, Location, CurrentPosition, Summary } = Resume.About;
+  const {
+    Name,
+    Location,
+    CurrentPosition,
+    Summary,
+    Email,
+    Website,
+    Phone
+  } = Resume.About;
   const NameArray = Name.split(" ");
   return (
     <div className="rezume-rezume">
+      <div className="rezume-rezume-contact">
+        <p>
+          Email:{" "}
+          <strong>
+            <a href={"mailto:" + Email}>{Email}</a>
+          </strong>
+        </p>
+        <p>
+          Website:{" "}
+          <strong>
+            <a
+              href={"http://" + Website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {Website}
+            </a>
+          </strong>
+        </p>
+        <p>
+          Phone: <strong>{Phone}</strong>
+        </p>
+      </div>
       <h2>
         <span>{NameArray.shift()}</span> {NameArray.join(" ")}
       </h2>
