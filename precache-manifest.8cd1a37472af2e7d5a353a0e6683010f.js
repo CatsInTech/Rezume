b5891fb44837b22e5bd85f73dbb87fd4f1d4bedb
Prajaram <prajaram345@gmail.com>
2019-11-16 12:30:03 +0000

    Updates

---
+++ precache-manifest.8cd1a37472af2e7d5a353a0e6683010f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91b398477ef8178883bae262cd24708",
+    "url": "/React-Resume-Builder/index.html"
+  },
+  {
+    "revision": "ddb0e35e6b3674603f54",
+    "url": "/React-Resume-Builder/static/css/main.7a9f64b1.chunk.css"
+  },
+  {
+    "revision": "ffa8435e73a8b6bb5e4a",
+    "url": "/React-Resume-Builder/static/js/2.b6fc503d.chunk.js"
+  },
+  {
+    "revision": "ddb0e35e6b3674603f54",
+    "url": "/React-Resume-Builder/static/js/main.a7e523ea.chunk.js"
+  },
+  {
+    "revision": "13458eabc6aea01536d6",
+    "url": "/React-Resume-Builder/static/js/runtime-main.75e4adaf.js"
+  }
+]);
