05ab7fe8070252440782a61ce25e239bd4eb5d54
Prajaram <prajaram345@gmail.com>
2019-11-16 15:16:25 +0000

    Added form component.

---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Forms = ({ CurrentPage }) => {
+  return <div className="shadow-sm bg-white p-2">{CurrentPage}</div>;
+};
+
+export default Forms;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Rezume from "../Rezume/Rezume";
+import Forms from "../Forms/Forms";
 
 const Main = ({ CurrentPage, Resume }) => {
   return (
-    <div className="container-fluid rg-main">
+    <div className="container-fluid rezume-main">
       <div className="row">
         <div className="col-3">
-          <div className="shadow-sm bg-white p-2">{CurrentPage}</div>
+          <Forms CurrentPage={CurrentPage} />
         </div>
         <div className="col-9">
           <Rezume Resume={Resume} />
