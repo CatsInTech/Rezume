510c425a39280e8687f2dbcbdcc67279e7c8e076
Prajaram <prajaram345@gmail.com>
2019-11-16 16:34:26 +0000

    Updated title branding.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import NavLink from "./_NavLink";
 const Header = ({ ChangePage, CurrentPage }) => {
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-between rezume-header">
-      <span className="navbar-brand mb-0 h1 col-1">React Resume Generator</span>
+      <span className="navbar-brand mb-0 h1 col-1">Rezume</span>
       <ul className="navbar-nav">
         {["About", "Sections", "Contact", "Design"].map(NavItem => (
           <NavLink
