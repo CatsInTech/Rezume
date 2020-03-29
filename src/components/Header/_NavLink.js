53c03ff32766db99b40df77a9a7bc40297279577
Praveen Kumar <praveenscience@gmail.com>
2020-03-29 17:05:43 +0100

    Updated nav to be semantically correct.
    * Changed span tag to button tag.
    * Updated SCSS to match the original style.
    
    Fixes #11.

--- src/components/Header/_NavLink.js
+++ src/components/Header/_NavLink.js
@@ -3,9 +3,9 @@ import React from "react";
 const NavLink = ({ Name, onClick, Active }) => {
   return (
     <li className={"nav-item" + (Active ? " active" : "")}>
-      <span className="nav-link" onClick={onClick}>
+      <button className="nav-link" onClick={onClick}>
         {Name}
-      </span>
+      </button>
     </li>
   );
 };
