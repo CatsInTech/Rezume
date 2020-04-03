f13b824d8fe9b87092296831e932b2504f0eec60
imabp <abir.pal899@gmail.com>
2020-04-03 13:57:22 +0530

    Added Link to the brand name in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import NavLink from "./_NavLink";
 const Header = ({ ChangePage, CurrentPage, Resume }) => {
   return (
     <nav className="navbar navbar-expand navbar-dark justify-content-between rezume-header">
-      <span className="navbar-brand mb-0 h1 col-1">Rezume</span>
+      <span className="navbar-brand mb-0 h1 col-1"><a style={{"text-decoration":"none","color":"white"}} href="/">Rezume</a></span>
       <ul className="navbar-nav">
         {Object.keys(Resume).map(NavItem => (
           <NavLink
