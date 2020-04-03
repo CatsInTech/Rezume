605b3d5c8ac27d143a226e2691ae4c85fa4759a0
aldrin163 <aldrinjenson@gmail.com>
2020-04-03 23:20:56 +0530

    Added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="page-footer font-small pt-4">
+      <div className="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        <a href="https://github.com/CatsInTech/Rezume"> Rezume</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
