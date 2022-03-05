b2007b512e6e917901a2e30201b1613343750fcd
Sukriti-m <sukritimaurya.fzd@gmail.com>
2022-03-05 19:36:16 +0530

    added styles and transitions to about us

--- src/components/Cards/CardText.js
+++ src/components/Cards/CardText.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function CardText(props){
     
-        return( <span>
+        return( <span className="card-text">
 <h2>{props.title}</h2><br />
 <p>{props.content} <a href={props.link}>{props.link}</a></p>
         </span>);
