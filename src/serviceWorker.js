1292fd163684e4292beb2a58deb0544cdb76f015
Mayonk Kumar Behera <mayonk.kumar9@gmail.com>
2022-03-15 14:36:12 +0530

    formatted the code according to the project requirement

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,7 +11,7 @@
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === "[::1]" ||
     // 127.0.0.1/8 is considered localhost for IPv4.
