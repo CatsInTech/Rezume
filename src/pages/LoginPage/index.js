0d7af4dc038a6d7a648a27cd42318cfab08a7661
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-04-11 12:04:06 +0530

    Validation added

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -3,31 +3,59 @@ import "./index.scss";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const blankStr = " ";
+  const [emailError, setEmailError] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState("");
+
+  function handleSubmit() {
+    if (email === "") {
+      setEmailError("Required");
+    } else {
+      setEmailError("");
+    }
+    if (password === "") {
+      setPasswordError("No Password provided");
+    } else {
+      setPasswordError("");
+    }
+  }
+
   return (
     <div className="container">
       <div className="loginform">
         <h1>Log In</h1>
-        <div>
+        <div className="emailField">
           <input
-            className="input"
+            className="inputEmail"
             type="email"
             placeholder="Email"
             autocomplete="off"
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
+          <div className="alert">{emailError}</div>
         </div>
         <div>
           <input
-            className="input"
+            className="inputPassword"
             type="password"
             placeholder="Password"
             autocomplete="off"
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
+          <div className="alert">{passwordError}</div>
         </div>
         <div className="Submit-form">
           <div>
-            <input className="inputSubmit" type="submit" value="Login" />
+            <input
+              className="inputSubmit"
+              type="submit"
+              value="Login"
+              onClick={handleSubmit}
+            />
           </div>
           <div>
             <Link to="/Signup">New User ?</Link>
